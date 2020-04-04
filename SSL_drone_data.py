57a7c5a2f4d815511f02e3890dc21333c11759b9
guidoAI <guido.de.croon@gmail.com>
2020-04-04 22:37:49 +0200

    Added the learning part and the fusion question

--- SSL_drone_data.py
+++ SSL_drone_data.py
@@ -10,6 +10,8 @@ Load data and process it for an SSL experiment.
 import pickle
 from matplotlib import pyplot as plt
 import numpy as np
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.linear_model import LinearRegression
 
 def pressure_to_height(pressure, base_height = 0):
     """ Transform pressure values from a Paparazzi log to height in meters.
@@ -49,7 +51,78 @@ def plot_pressure(drone_data):
     plt.plot(drone_data['pressure'])
     plt.xlabel('Time [-]')
     plt.ylabel('Pressure [Pa]')
+
+def train_regression_KNN(training_data, target_values, k = 5, wghts = 'uniform'):
+    """
+    - training_data: [n_samples, n_features]
+    - target_values: [n_samples, n_outputs]
+    - k: number of neighbors
+    - wghts:    'uniform' = all neighbors contribute equally, 
+                'distance' = weights points by the inverse of their distance
+    """
+    kNN = KNeighborsRegressor(n_neighbors = k, weights = wghts)
+    kNN.fit(training_data, target_values)
+    return kNN
     
+def predict_regression_KNN(kNN, test_data):
+    
+    outputs = kNN.predict(test_data)
+    
+    return outputs
 
+def map_pressure_to_sonar(drone_data, training_ratio=0.8, method = 'knn'):
 
+    train_ind = int(training_ratio * len(drone_data['pressure']))
+    training_data = drone_data['pressure'][:train_ind]
+    training_targets = drone_data['sonar'][:train_ind]
+    test_data = drone_data['pressure'][train_ind:]
+    test_targets = drone_data['sonar'][train_ind:]
+    
+    if(method == 'knn'):
+        kNN = train_regression_KNN(training_data, training_targets)
+        training_outputs = predict_regression_KNN(kNN, training_data)
+        test_outputs = predict_regression_KNN(kNN, test_data)
+    else:
+        lr = LinearRegression().fit(training_data, training_targets)
+        training_outputs = lr.predict(training_data)
+        test_outputs = lr.predict(test_data)
+    
+    plt.figure()
+    plt.plot(range(len(test_data)), test_outputs)
+    plt.plot(range(len(test_data)), test_targets)
+    plt.legend(['Pressure-based estimates','Sonar measurements'])
+    
+    plt.figure()
+    plt.hist(test_outputs-test_targets)
+    plt.title('Histogram of $h_{pressure}$ - $h_{sonar}$')
+    
+    print('Mean absolute error = {:.4f}, mean error = {:.4f}'.format(np.mean(abs(test_outputs-test_targets)), np.mean(test_outputs-test_targets)))
+    
+    # Fusion:
+    test_ground_truth = drone_data['optitrack'][train_ind:]
+    training_ground_truth = drone_data['optitrack'][:train_ind]
+    
+    # SSL:
+    var_pressure = np.var(training_outputs-training_targets)
+    # A priori knowledge:
+    # var_pressure = np.var(training_outputs - training_ground_truth)
+    var_sonar = np.var(training_data - training_ground_truth)
+    test_fused = (var_sonar * test_outputs + var_pressure * test_targets) / (var_sonar + var_pressure)
+    print('MAE sonar: {:.4f}, MAE learned pressure = {:.4f}, MAE fused = {:.4f}'.format(
+          np.mean(abs(test_targets - test_ground_truth)), np.mean(abs(test_outputs - test_ground_truth)),
+          np.mean(abs(test_fused - test_ground_truth)) ))
+    
+    var_t = np.var(training_ground_truth)
+    mean_t = np.mean(training_ground_truth)
+    print('Mean t = {}'.format(mean_t))
+    test_fused = (var_sonar * test_outputs + var_pressure * test_targets) / (var_sonar + var_pressure + var_t)
+    print('MAE sonar: {:.4f}, MAE learned pressure = {:.4f}, MAE fused = {:.4f}'.format(
+          np.mean(abs(test_targets - test_ground_truth)), np.mean(abs(test_outputs - test_ground_truth)),
+          np.mean(abs(test_fused - test_ground_truth)) ))
+    
+    
+if __name__ == "__main__":
+    drone_data = get_drone_data()
+    map_pressure_to_sonar(drone_data, method = 'knn')
+    
 
